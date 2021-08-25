981a30a0f4756f0a1ad629ae2ddf652baad17873
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-25 05:46:42 +0400

    Add jobocity project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7edad4cc16a3bbdb754b";
+/******/ 	var hotCurrentHash = "c0890cbd058f42879550";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
